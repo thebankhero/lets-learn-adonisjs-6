de31e1ce31980af94cadf790ad6f7cf17461f34f
Nutthapon Sangtai <thebankhero@gmail.com>
2025-07-01 20:04:38 +0700

    Listing and Counting a Writer's Movies

--- start/routes.ts
+++ start/routes.ts
@@ -6,8 +6,10 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-const DirectorsController = () => import('#controllers/directors_controller')
 import router from '@adonisjs/core/services/router'
+
+const WritersController = () => import('#controllers/writers_controller')
+const DirectorsController = () => import('#controllers/directors_controller')
 const RedisController = () => import('#controllers/redis_controller')
 const MoviesController = () => import('#controllers/movies_controller')
 
@@ -17,8 +19,12 @@ router
   .get('/movies/:slug', [MoviesController, 'show'])
   .as('movies.show')
   .where('slug', router.matchers.slug())
+
 router.get('/directors', [DirectorsController, 'index']).as('directors.index')
 router.get('/directors/:id', [DirectorsController, 'show']).as('directors.show')
+
+router.get('/writers', [WritersController, 'index']).as('writers.index')
+router.get('/writers/:id', [WritersController, 'show']).as('writers.show')
 
 router.delete('/redis/flush', [RedisController, 'flush']).as('redis.flush')
 router.delete('/redis/:slug', [RedisController, 'destroy']).as('redis.destroy')
