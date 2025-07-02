d6b3ffdb1a4fe2598b31378499189dac8038fbda
Nutthapon Sangtai <thebankhero@gmail.com>
2025-07-02 20:00:56 +0700

    Many-To-Many Model Factory Relationships

--- database/seeders/01_fake_seeder.ts
+++ database/seeders/01_fake_seeder.ts
@@ -36,7 +36,17 @@ export default class extends BaseSeeder {
       index++
     }).createMany(movies.length)
 
-    await MovieFactory.with('director').with('writer').createMany(3)
+    await MovieFactory.with('director')
+      .with('writer')
+      .with('castMembers', 3, (builder) =>
+        builder.pivotAttributes([
+          { character_name: 'Rober', sort_order: 0 },
+          { character_name: 'Joy', sort_order: 1 },
+          { character_name: 'Anna', sort_order: 2 },
+        ])
+      )
+      .with('crewMembers', 5, (builder) => builder.pivotAttributes([{ title: 'Camera Operator' }]))
+      .createMany(3)
     await MovieFactory.with('director').with('writer').apply('released').createMany(2)
     await MovieFactory.with('director').with('writer').apply('releasingSoon').createMany(2)
     await MovieFactory.with('director').with('writer').apply('postProduction').createMany(2)
