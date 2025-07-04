c6b9e100d700f08f8b43f78a2274979cff86de03
Nutthapon Sangtai <thebankhero@gmail.com>
2025-07-04 20:54:37 +0700

    Logging Out An Authenticated User

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,7 @@
 */
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const LogoutController = () => import('#controllers/auth/logouts_controller')
 
 const LoginController = () => import('#controllers/auth/login_controller')
 const RegisterController = () => import('#controllers/auth/register_controller')
@@ -45,6 +46,8 @@ router
 
     router.get('/login', [LoginController, 'show']).as('login.show').use(middleware.guest())
     router.post('/login', [LoginController, 'store']).as('login.store').use(middleware.guest())
+
+    router.post('/logout', [LogoutController, 'handle']).as('logout').use(middleware.auth())
   })
   .prefix('/auth')
   .as('auth')
